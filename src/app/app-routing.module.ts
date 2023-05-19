157af29faca08c506a63555dd2f2d8944986d8dd
Metalnd01 <williamkurtmorales@gmail.com>
2023-05-19 12:33:42 -0300

    agrega nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AutosComponent } from './components/autos/autos.component';
+import { PrestamosComponent } from './components/prestamos/prestamos.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "login", component: LoginComponent}
+  {path: "login", component: LoginComponent},
+  {path: "cars", component: AutosComponent},
+  {path: "loan", component: PrestamosComponent}
 ];
 
 @NgModule({
